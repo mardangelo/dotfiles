a436d242e3f9b2d0a9de2534da9bb584fa01ccbf
mars <mar@dangelo.sh>
2025-04-06 22:52:51 -0400

    Use C-g as ESC in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_set_keymap("n", "<C-g>", "<Esc>", { noremap = true })
+vim.api.nvim_set_keymap("i", "<C-g>", "<Esc>", { noremap = true })
+vim.api.nvim_set_keymap("v", "<C-g>", "<Esc>", { noremap = true })
