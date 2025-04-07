5c55b00198548609b9256cbc061efa7c2d152a6b
mars <mar@dangelo.sh>
2025-04-07 00:43:42 -0400

    Search and navigation based on completion function with Consult

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -178,6 +178,92 @@
   (completion-category-defaults nil)
   (completion-category-overrides '((file (styles basic partial-completion)))))
 
+;; Search and navigation based on emacs completing function, can quickly select an item from a list of candidates
+(use-package consult
+  :bind (;; C-c bindings in `mode-specific-map`
+	 ("C-c M-x" . consult-mode-command)
+	 ("C-c h" . consult-history)
+	 ("C-c k" . consult-kmacro)
+	 ("C-c m" . consult-man)
+	 ("C-c i" . consult-info)
+	 ([remap Info-search] . consult-info)
+	 ;; C-x bindings in `ctl-x-map`
+	 ("C-x M-:" . consult-complex-command)
+	 ("C-x b" . consult-buffer)
+	 ("C-x 4 b" . consult-buffer-other-window)
+	 ("C-x 5 b" . consult-buffer-other-frame)
+	 ("C-x t b" . consult-buffer-other-tab)
+	 ("C-x r b" . consult-bookmark)
+	 ("C-x p b" . consult-project-buffer)
+	 ;; Custom M-# bindings for fast register
+	 ("M-#" . consult-register-load)
+	 ("M-'" . consult-register-store)
+	 ("C-M-#" . consult-register)
+	 ;; Other custom bindings
+	 ("M-y" . consult-yank-pop)
+	 ;; M-g bindings in `goto-map`
+	 ("M-g e" . consult-compile-error)
+	 ("M-g f" . consult-flymake)
+	 ("M-g g" . consult-goto-line)
+	 ("M-g M-g" . consult-goto-line)
+	 ("M-g o" . consult-outline)
+	 ("M-g m" . consult-mark)
+	 ("M-g k" . consult-global-mark)
+	 ("M-g i" . consult-imenu)
+	 ("M-g I" . consult-imenu-multi)
+	 ;; M-s bindings in `search-map`
+	 ("M-s d" . consult-find)
+	 ("M-s c" . consult-locate)
+	 ("M-s g" . consult-grep)
+	 ("M-s G" . consult-git-grep)
+	 ("M-s r" . consult-ripgrep)
+	 ("M-s l" . consult-line)
+	 ("M-s L" . consult-line-multi)
+	 ("M-s k" . consult-keep-lines)
+	 ("M-s u" . consult-focus-lines)
+	 ;; Isearch integration
+	 ("M-s e" . consult-isearch-history)
+	 :map isearch-mode-map
+	 ("M-e" . consult-isearch-history)
+	 ("M-s e" . consult-isearch-history)
+	 ("M-s l" . consult-line)
+	 ("M-s L" . consult-line-multi)
+	 ;; Minibuffer history
+	 :map minibuffer-local-map
+	 ("M-s" . consult-history)
+	 ("M-r" . consult-history))
+  ;; Enable automatic preview at point in the *Completions* buffer. Relevant for default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+  :init
+  ;; Tweak register preview for `consult-register-load`, `consult-register-store` and the built-in commands. Improves register formatting, adds thin separator lines, register sorting, and hides the window mode line.
+  (advice-add #'register-preview :override #'consult-register-window)
+  (setq register-preview-delay 0.5)
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+	xref-show-definitions-function #'consult-xref)
+  :config
+  ;; Configure preview. The default value is 'any, such that any key triggers preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep consult-man
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key `M-.`
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Configure the narrowing key.
+  (setq consult-narrow-key "<") ;; `C-+` works equally well
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (keymap-set consult-narrow-map (concat consult-narrow-key " ?") #'consult-narrow-help)
+  )
+
 ;; Enable rich minibuffer annotations
 (use-package marginalia
   :bind (:map minibuffer-local-map
