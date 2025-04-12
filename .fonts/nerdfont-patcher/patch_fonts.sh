77c7c3089e37d9acba36c05de6361e37bad7daeb
mars <mar@dangelo.sh>
2025-04-12 16:06:51 -0400

    Update font patching script and ignore in/out paths

--- .fonts/nerdfont-patcher/patch_fonts.sh
+++ .fonts/nerdfont-patcher/patch_fonts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker run --rm -v ./original:/in -v ./patched:/out nerdfonts/patcher \
-  --complete --single-width-glyphs --adjust-line-height --progressbars
+  --complete --single-width-glyphs --adjust-line-height --progressbars --makegroup 6
