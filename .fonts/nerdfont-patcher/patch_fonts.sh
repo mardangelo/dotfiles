8d11068489d98f2af0014aa3e189ffe574ebdf24
mars <mar@dangelo.sh>
2025-04-12 23:37:35 -0400

    Use up to date docker image for font patcher

--- .fonts/nerdfont-patcher/patch_fonts.sh
+++ .fonts/nerdfont-patcher/patch_fonts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker run --rm -v ./original:/in -v ./patched:/out nerdfonts/patcher \
-  --complete --single-width-glyphs --adjust-line-height --progressbars --makegroup 6
+sudo docker run --rm -v ./input:/input -v ./output:/output ghcr.io/cdalvaro/docker-nerd-fonts-patcher:latest \
+  --complete --mono --adjust-line-height --no-progressbars --makegroup 6 --quiet
